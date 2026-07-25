87afb6678b87e53a97a3b9243ab5ef96aecf784f
MUNACHIW <munasom25@gmail.com>
2026-07-25 18:08:59 +0100

    wildcard hosting

--- american_redcross/settings.py
+++ american_redcross/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-0h36qrkne2hkrv0@i+wry1#)d2l1=ldbyz-9as=tmf5a*!80-+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
